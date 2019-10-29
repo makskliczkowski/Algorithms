3f23b383e4bf7c63fa19ed5572ea82f401bf30dc
Maksymilian Kliczkowski <48489493+makskliczkowski@users.noreply.github.com>
2019-10-29 22:44:26 +0100

    Initial

---
+++ Algorytmika/3/ConsoleApplication1/ConsoleApplication1/pch.h
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <math.h>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <exception>
+#include <istream>
+#include <ostream>
+#include <vector>
+#include <assert.h>
+#include <ctime>
+#include <algorithm>
+using namespace std; 
+#ifndef PCH_H
+#define PCH_H
+
+class BigInteger {
+private:
+	string number;
+	vector<int> vnumber;
+	int factor = 10;//for unit representation;
+	int length;
+	int sign;
+
+public:
+	BigInteger(std::string stringNumber);//
+	BigInteger(int integerNumber);
+	BigInteger();
+	BigInteger(const BigInteger &a);
+	~BigInteger();
+	void print();//put out to stream
+	void setstring(string stringNumber);//when we put ()constructor
+	BigInteger changesign();//for sign change
+	void numberDelete();
+	void removeZeros();
+	void random(int length);
+
+
+	//friends
+	friend BigInteger mulIntStringByChar(const BigInteger& op1, const char op2);
+	friend bool isZero(const BigInteger& oper);//(2*O(n))
+	friend BigInteger multiplyTime(const BigInteger &a, const BigInteger &b, int &t, int&length);
+	//arithmetic
+	BigInteger add(BigInteger bigInt);//functions that can be replaced by operators
+	BigInteger subtruct(BigInteger bigInt);
+	BigInteger mod();
+	BigInteger multiply(BigInteger bigInt);
+	BigInteger power(int n);
+	BigInteger divide(BigInteger const &bigInt);
+	BigInteger factorial();//silnia
+	
+
+	//operations overload
+	friend BigInteger operator +(const BigInteger &a, const BigInteger &b);
+	friend BigInteger operator +(const BigInteger &a, const string &b);
+	friend BigInteger operator -(const BigInteger &a, const BigInteger &b);
+	friend BigInteger operator -(const BigInteger &a, const string &b);
+	friend BigInteger operator *(const BigInteger &a, const BigInteger &b);
+	friend BigInteger operator *(const BigInteger &a, const string &b);
+	friend BigInteger operator /(const BigInteger &a, const BigInteger &b);
+	friend BigInteger operator /(const BigInteger &a, const string &b);
+	BigInteger & operator -();//change sign
+	BigInteger & operator = (const BigInteger & a); 
+	BigInteger & operator = (const string &a);
+	BigInteger & operator = (const long long &a);
+	BigInteger & operator +=(const BigInteger &bigInt); 
+	BigInteger & operator -=(const BigInteger &bigInt);
+	BigInteger & operator *=(const BigInteger &bigInt);
+	BigInteger & operator /=(const BigInteger &bigInt);
+	friend bool operator <(const BigInteger &a, const BigInteger &b);/** logical operators*/
+	friend bool operator <=(const BigInteger &a, const BigInteger &b);
+	friend bool operator >=(const BigInteger &a, const BigInteger &b);
+	friend bool operator >(const BigInteger &a, const BigInteger &b);
+	friend bool operator ==(const BigInteger &a, const BigInteger &b);
+	friend bool operator !=(const BigInteger &a, const BigInteger &b);
+	char operator[](int n);
+	friend ostream & operator <<(ostream & out, const BigInteger &a);/**\fn operator  ostream*/
+	friend istream & operator >>(istream & in, BigInteger &a); /*data istream operator*/
+};
+void Karatsuba(int average, int max_n, string file_name);
+
+#endif //PCH_H
---
+++ Algorytmika/4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,58 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	srand(time(NULL));// seeed for functions
+	try {
+		ofstream a;
+		a.open("a.dat");
+		if (!a.is_open()) throw "Unable to open file\n";
+		ofstream b;
+		b.open("b.dat");
+		if (!b.is_open()) throw "Unable to open file\n";
+		ofstream sz;
+		sz.open("sz.dat");
+		if (!sz.is_open()) throw "Unable to open file\n";
+		//file handling
+
+		int average = 1000;
+		float k1= 0;//number of balls
+		float k2 = 0;//number of balls
+		float k3 = 0;
+		Urn temp(5,0);
+		 //a)b)
+		for (int n = 5; n < 125; n++) 
+		{
+			k1 = 0;
+			k2 = 0;
+			k3 = 0;
+			for (int av = 0; av < average; av++)
+			{
+				temp.isAtLeast(1);
+				k1 = k1 + temp.thisBall();
+				temp.isInOne(2);
+				k2 = k2 + temp.thisBall();
+				k3=k3+temp.howManyEmpt();
+
+			}
+			k1 = k1 / average;
+			k2 = k2 / average;
+			k3 = k3 / average;
+			a << temp.thisUrns() << "\t" << k1 << endl;
+			b << temp.thisUrns() << "\t" << k2 << endl;
+			sz << temp.thisUrns() << "\t" << k3 << endl;
+			temp.addUrn(1);
+		}
+		a.close();
+		b.close();
+		sz.close();
+		return 0;
+	}
+	catch(...){}
+
+}
+
